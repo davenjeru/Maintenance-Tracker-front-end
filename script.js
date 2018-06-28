4cfab49dc3e76c466337baf9d94f3400231d2401
Dave Mathews Njeru <davenmathews@gmail.com>
2018-06-28 19:09:33 +0300

    Feature #158594101 Admin Dashboard front end
    - add code that fetches requests and displays them on the
      dashboard page
    - add code to respond to requests on button click
    [Finishes #158594101]
    
    Signed-off-by: Dave Mathews Njeru <davenmathews@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 export const url = "http://localhost:5000/api/v1";
+
+export function compare(a, b) {
+    /* this is the function that compares requests by id and reverses the requests
+        array received in the response
+        */
+    if (a.request_id > b.request_id) {
+        return -1;
+    }
+    if (a.request_id < b.request_id) {
+        return 1;
+    }
+    // a must be equal to b
+    return 0;
+}
